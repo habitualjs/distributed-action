7d7ffcd4b5524855ea757ef41b8c7728df442cac
Damon Barrett <habitualjs@gmail.com>
2021-02-04 13:49:53 -0500

    Added default class implementation. Exported all types.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
-interface StageClient<Event> {
+export interface StageClient<Event> {
     send(events: Event[]): void
 }
 
-interface Downstream {
+export interface Downstream {
     get<T>(stageName: string): Promise<StageClient<T> | undefined>
 }
 
-export default interface Action<Event = any> {
+export interface Action<Event = any> {
     validate(event: any): event is Event
     handle(event: Event, downstream: Downstream): void | Promise<void>
     flush?(stageName: string, events: Event[]): number
+}
+
+export default class {
+    validate(event: any) {
+        return true 
+    }
+
+    handle(event: Event, downstream: Downstream) {
+        return
+    }
+
+    flush(stageName: string, events: Event[]) {
+        return 0
+    }
 }
